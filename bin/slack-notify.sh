78a37479dd65033a32181906d2273e4c792761e5
Evan Chen <daevanchen@gmail.com>
2025-07-12 23:49:10 +0800

    refactor: make slack-notify.sh leverage slack-notify-channel.sh

--- bin/slack-notify.sh
+++ bin/slack-notify.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-. ~/.secrets/slack
-
-curl \
-    --data-urlencode "text=$1" \
-    --data-urlencode "token=$SLACK_TOKEN" \
-    --data-urlencode 'channel=headsup' \
-    https://slack.com/api/chat.postMessage
+slack-notify-channel.sh headsup "$1"
